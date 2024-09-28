b6fcef4111f75ec7002c44245e53b75c0770c16f
Douglas Reis <doreis@lowrisc.org>
2024-09-28 17:40:25 +0100

    [test] Create an unitest environment
    Signed-off-by: Douglas Reis <doreis@lowrisc.org>

--- tests/main.cc
+++ tests/main.cc
@@ -1,18 +1,23 @@
+#include <gtest/gtest.h>
 #include "fmt.hh"
 #include <iostream>
 #include <cstring>
+#include <string>
 
-struct IostreamWrapper {
-  void write(const char *buf, size_t n) {
-    if (strlen(buf) < n) {
-      std::cout << buf;
-    }
-  }
+struct IostreamMock {
+  std::array<char, 64> buf_{0};
+  void write(const char *buf, size_t n) { std::copy(buf, buf + n, buf_.begin()); }
 };
 
-int main(void) {
-  auto wrapper = IostreamWrapper();
-  reisfmt::Fmt<IostreamWrapper, 64> fmt(wrapper);
-  fmt.print("hello world", 1);
-  return 0;
+TEST(Fmt, simple_string) {
+  auto mock = IostreamMock();
+  reisfmt::Fmt<IostreamMock, 64> fmt(mock);
+  std::string msg("Hello World!!");
+  fmt.print(msg.c_str());
+  EXPECT_EQ(mock.buf_.data(), msg);
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
