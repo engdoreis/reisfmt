5f4164ad4ae824e956dd301e18438c41fe4d107b
Douglas Reis <doreis@lowrisc.org>
2024-09-29 20:07:24 +0100

    [feature] Format integer arguments
    Signed-off-by: Douglas Reis <doreis@lowrisc.org>

--- lib/fmt.hh
+++ lib/fmt.hh
@@ -15,15 +15,71 @@ class Fmt {
  private:
   T &device;
   std::array<char, N> buf;
+  const char *fmt_str = nullptr;
 
  public:
-  Fmt(T &device) : device(device){};
+  Fmt(T &device) : device(device) {};
 
+ private:
+  // Base case to stop the recursion.
+  void format() {
+    if (fmt_str && *fmt_str) {
+      auto len = strlen(fmt_str);
+      device.write(fmt_str, len);
+      fmt_str += len;
+    }
+  }
+
+  template <typename U, typename... Args>
+  void format(U first, Args... rest) {
+    while (fmt_str && *fmt_str) {
+      if (*fmt_str == '{') {
+        to_str(buf, first);
+        device.write(buf.data(), strlen(buf.data()));
+
+        while (*(fmt_str++) == '}');
+        fmt_str++;
+        format(rest...);
+      } else {
+        device.write(fmt_str++, 1);
+      }
+    }
+  }
+
+ public:
   template <typename... Args>
   void print(const char *fmt, Args... args) {
-    auto last = fmt + strlen(fmt);
-    std::copy(fmt, last, buf.begin());
-    device.write(buf.data(), strlen(fmt));
+    fmt_str = fmt;
+    format(args...);
+    fmt_str = nullptr;
+  }
+
+  template <size_t SIZE, typename U>
+    requires std::integral<U>
+  static inline char *to_str(std::array<char, SIZE> &buf, U num) {
+    size_t head = 0;
+    size_t tail = SIZE - 1;
+    if constexpr (std::signed_integral<U>) {
+      // This code won't be linked for unsigned U.
+      if (num < 0) {
+        buf[head++] = '-';
+        num         = 0 - num;
+      }
+    }
+
+    for (; tail > 0 && num > 0; --tail) {
+      buf[tail] = num % 10 + '0';
+      num /= 10;
+    }
+
+    tail++;
+    size_t len = SIZE - tail + head;
+    for (size_t i = 0; i < len; ++i) {
+      buf[head + i] = buf[tail + i];
+    }
+
+    buf[len] = 0;
+    return buf.data();
   }
 };
 };  // namespace reisfmt
--- tests/main.cc
+++ tests/main.cc
@@ -3,18 +3,55 @@
 #include <iostream>
 #include <cstring>
 #include <string>
+#include <format>
 
 struct IostreamMock {
-  std::array<char, 64> buf_{0};
-  void write(const char *buf, size_t n) { std::copy(buf, buf + n, buf_.begin()); }
+  std::vector<char> buf_;
+  void write(const char *buf, size_t n) {
+    while (n--) {
+      buf_.push_back(*buf++);
+    }
+  }
+
+  std::string to_string() { return std::string(buf_.begin(), buf_.end()); }
 };
 
-TEST(Fmt, simple_string) {
-  auto mock = IostreamMock();
-  reisfmt::Fmt<IostreamMock, 64> fmt(mock);
+class FmtTest : public testing::Test {
+ public:
+  IostreamMock mock_ = IostreamMock();
+  reisfmt::Fmt<IostreamMock, 64> fmt_;
+
+  FmtTest() : fmt_(mock_) {}
+};
+
+TEST_F(FmtTest, simple_string) {
   std::string msg("Hello World!!");
-  fmt.print(msg.c_str());
-  EXPECT_EQ(mock.buf_.data(), msg);
+  fmt_.print(msg.c_str());
+  EXPECT_EQ(mock_.to_string(), msg);
+}
+
+TEST_F(FmtTest, pos_int) {
+  constexpr const char *msg = "{} * {} = {}...";
+  fmt_.print(msg, 10, 20, 10 * 20);
+  EXPECT_EQ(mock_.to_string(), std::format(msg, 10, 20, 10 * 20));
+}
+
+TEST_F(FmtTest, neg_int) {
+  constexpr const char *msg = "{} * {} = {}...";
+  fmt_.print(msg, -10, 20, -10 * 20);
+  EXPECT_EQ(mock_.to_string(), std::format(msg, -10, 20, -10 * 20));
+}
+
+TEST_F(FmtTest, int_arg_execess) {
+  constexpr const char *msg = "{} * {} ";
+  fmt_.print(msg, 10, 20, 10 * 20);
+  EXPECT_EQ(mock_.to_string(), std::format(msg, 10, 20, 10 * 20));
+}
+
+TEST_F(FmtTest, int_arg_missing) {
+  constexpr const char *msg = "{} * {} = {}";
+  fmt_.print(msg, 10, 20);
+  EXPECT_EQ(mock_.to_string(), "10 * 20 = {}");
 }
 
 int main(int argc, char **argv) {
