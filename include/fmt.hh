0606a740e357fdc80b50734bad0451e414e94ff7
Douglas Reis <doreis@lowrisc.org>
2025-12-26 18:01:32 +0000

    [refactor] Move concept to it's own file

--- include/fmt.hh
+++ include/fmt.hh
@@ -10,13 +10,9 @@
 
 #include "to_string.hh"
 #include "spec.hh"
+#include "writeable.hh"
 
 namespace reisfmt {
-
-template <typename T>
-concept Writeable = requires(T t, const char *buf, size_t n) {
-  { t.write(buf, n) } -> std::same_as<void>;
-};
 
 // Foward declaration.
 template <Writeable T>
